e23ab6c02ae7d3de86f28ba4367463f511e4f137
Kristina Sviridova <KS.Kristina-2000@yandex.ru>
2025-11-22 14:53:30 +0300

    #1. Точка координат

--- src/main/java/ru/stepup/payments/Main.java
+++ src/main/java/ru/stepup/payments/Main.java
@@ -4,6 +4,13 @@ package ru.stepup.payments;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Point point1 = new Point(1, 3);
+        Point point2 = new Point(1, 3);
+        Point point3 = new Point(5, 8);
+        System.out.println(point1);
+        System.out.println(point2);
+        System.out.println(point3);
+        System.out.println(point1==point2);
+        System.out.println(point1==point3);
     }
 }
